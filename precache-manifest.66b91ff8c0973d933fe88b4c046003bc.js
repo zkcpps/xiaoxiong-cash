e5244cb6a1fb924328af8999c738cafa5e288095
zhangkaicheng <1129820657@qq.com>
2021-07-19 14:44:12 +0800

    Updates

---
+++ precache-manifest.66b91ff8c0973d933fe88b4c046003bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc4494972ea3ef931c95c8e197516d5",
+    "url": "/xiaoxiong/index.html"
+  },
+  {
+    "revision": "5fd153d7dcfa10510918",
+    "url": "/xiaoxiong/static/css/main.1fd9c27f.chunk.css"
+  },
+  {
+    "revision": "1fd48787a4b8b7acda31",
+    "url": "/xiaoxiong/static/js/2.6c38c696.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/xiaoxiong/static/js/2.6c38c696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd153d7dcfa10510918",
+    "url": "/xiaoxiong/static/js/main.a50dedf0.chunk.js"
+  },
+  {
+    "revision": "0907e88df3215767c170",
+    "url": "/xiaoxiong/static/js/runtime-main.45c0dbcf.js"
+  }
+]);
